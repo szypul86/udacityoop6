423a2ee5a101efa912f1b1d5e4a6438c1cc9408f
Andrzej Szypulski <szypul86@gmail.com>
2019-06-27 12:16:55 +0200

    comments added to FileScanner

--- src/FileScanner.java
+++ src/FileScanner.java
@@ -3,17 +3,21 @@ import java.io.File;
 import java.util.Scanner;
 
 public class FileScanner {
+    // throws Excepitons added!
     public static void main(String [] args) throws Exception{
         File file = new File("book.txt");
+        // (file) instead of System.in to operate on file nit on terminal input
         Scanner scanner = new Scanner(file);
         int words = 0;
+        //unless the file has no more lines:
         while(scanner.hasNextLine()) {
+            //scan a line and store it in line variabl
             String line = scanner.nextLine();
+            // count all parts of the line splitted with " " and add to a words counter
             words += line.split(" ").length;
         }
 
-
-            System.out.println("the file has " + words + " words.");
+        System.out.println("the file has " + words + " words.");
     }
 
 
